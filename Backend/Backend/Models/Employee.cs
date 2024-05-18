1902a3eeba2940103cd091444cdc24366a5e8120
phanhao205 <haophan2052002@gmail.com>
2024-05-18 14:15:45 +0700

    build prj

---
+++ Backend/Backend/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string? Address { get; set; }
+
+    public byte? Gender { get; set; }
+
+    public DateTime BirthDay { get; set; }
+}
---
+++ Backend/Backend/Models/EmployeeManagementContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models;
+
+public partial class EmployeeManagementContext : DbContext
+{
+    public EmployeeManagementContext()
+    {
+    }
+
+    public EmployeeManagementContext(DbContextOptions<EmployeeManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC074E551411");
+
+            entity.ToTable("Employee");
+
+            entity.HasIndex(e => e.Phone, "UQ__Employee__5C7E359E441C6A37").IsUnique();
+
+            entity.HasIndex(e => e.Email, "UQ__Employee__A9D10534FDBE2399").IsUnique();
+
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.BirthDay).HasColumnType("date");
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Name).HasMaxLength(150);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
