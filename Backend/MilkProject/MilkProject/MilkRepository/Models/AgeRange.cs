b45b665e0825642d689c7376c5e9027933441807
Tadnuat <datdptse172916@fpt.edu.vn>
2024-06-03 20:13:36 +0700

    API
    Milk API

---
+++ Backend/MilkProject/MilkProject/MilkRepository/Models/AgeRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MilkRepository.Models;
+
+public partial class AgeRange
+{
+    public int AgeRangeId { get; set; }
+
+    public string? Baby { get; set; }
+
+    public string? Mama { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public virtual Product? Product { get; set; }
+}
